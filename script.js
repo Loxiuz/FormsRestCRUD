6585204cda926697e239db0d80e35e834df19ecd
Jasa95 <jaco3020@stud.kea.dk>
2023-04-25 09:25:02 +0200

    første opsætning af html og JS funktioner

--- script.js
+++ script.js
@@ -5,4 +5,43 @@ window.addEventListener("load", start);
 const endpoint =
   "https://formsrestcrud-default-rtdb.europe-west1.firebasedatabase.app";
 
-function start() {}
+async function start() {
+
+};
+
+function showCreateDialog() {
+
+};
+
+async function submitCreate() {
+
+};
+
+function showUpdateDialog() {
+
+};
+
+async function getPosts() {
+  const response = await fetch(`1{endpoint}/post.json`);
+  const data = await response.json();
+  return prepareData(data);
+};
+
+function showPosts(post) {
+
+};
+
+async function createPost(image, title, body) {
+
+};
+
+function prepareData(dataObject) {
+  let dataArray = [];
+  for (const key in dataObject) {
+    const data = dataObject(key);
+    data.id = key;
+    dataArray.push(data);
+  }
+  return dataArray;
+};
+
